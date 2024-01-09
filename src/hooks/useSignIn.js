1932b33d770dc5cc5c588b37ab97a67f51961350
ming xiong <iona.m.xiong@gmail.com>
2024-01-09 19:11:25 +0200

    10.15

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,8 +1,12 @@
 import { useMutation } from "@apollo/client";
 import { SIGN_IN } from "../graphql/mutations";
+import useAuthStorage from "./useAuthStorage";
+import { useApolloClient } from "@apollo/client";
 
 const useSignIn = () => {
   const [mutate, { data, loading, error }] = useMutation(SIGN_IN);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
 
   if (loading) {
     return "Loading...";
@@ -14,6 +18,10 @@ const useSignIn = () => {
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({ variables: { username, password } });
+    await authStorage.setAccessToken(data.authenticate.accessToken);
+    const accessToken = await authStorage.getAccessToken();
+    console.log("accessToken", accessToken);
+    apolloClient.resetStore();
     return data;
   };
 
