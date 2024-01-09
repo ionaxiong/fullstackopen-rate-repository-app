9b905433141f80f8c099b90c09d6d336c0164bca
ming xiong <iona.m.xiong@gmail.com>
2024-01-09 17:19:31 +0200

    10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  // ------- Implement the useRepositories hook with Apollo Client -------
+  const [repositories, setRepositories] = useState();
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+    onCompleted: (data) => {
+      setRepositories(data.repositories);
+    },
+  });
+
+  if (loading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    return `Error! ${error.message}`;
+  }
+
+  return { repositories, loading };
+
+  // ------- Implement the useRepositories hook with API calls -------
+  // const [repositories, setRepositories] = useState();
+  // const [loading, setLoading] = useState(false);
+
+  // const fetchRepositories = async () => {
+  //   setLoading(true);
+
+  //   const response = await fetch("http://192.168.50.188:5000/api/repositories");
+  //   const json = await response.json();
+
+  //   setLoading(false);
+  //   setRepositories(json);
+  // };
+
+  // useEffect(() => {
+  //   fetchRepositories();
+  // }, []);
+  // return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
